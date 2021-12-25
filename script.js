995c751aa557d26a721c3bf632c0625dcdab603e
Sam Smith <wagner.godinho@gmail.com>
2021-12-25 17:27:18 -0300

    home and dishes section development

--- script.js
+++ script.js
@@ -20,3 +20,17 @@ document.querySelector("#close").onclick = () =>{
 
 	document.querySelector('#search-form').classList.remove('active');
 }
+
+var swiper = new Swiper(".home-slider", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 7500,
+          disableOnInteraction: false,
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        loop:true,
+      });
