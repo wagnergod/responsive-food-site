874ef2bb61d04fb5ad3c5e51334599532e2bbf63
Sam Smith <wagner.godinho@gmail.com>
2021-12-29 21:41:21 -0300

    order and footer section development + js scripts

--- script.js
+++ script.js
@@ -4,11 +4,34 @@ let navbar = document.querySelector('.navbar');
 menu.onclick = () =>{
 	menu.classList.toggle('fa-times');
 	navbar.classList.toggle('active');
-} 
+}
+
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header .navbar a');
 
 window.onscroll = () =>{
 	menu.classList.remove('fa-times');
 	navbar.classList.remove('active');
+
+	section.forEach(sec =>{
+
+		let top = window.scrollY;
+		let height = sec.offsetHeight;
+		let offset = sec.offsetTop - 150;
+		let id = sec.getAttribute('id');
+
+		if(top >= offset && top < offset + height){
+			navLinks.forEach(links =>{
+
+				links.classList.remove('active');
+				document.querySelector('header .navbar a[href*='+id+']').classList.add('active');
+			});
+		};
+	
+	});
+
+
+
 } 
 
 document.querySelector("#search-icon").onclick = () =>{
@@ -58,3 +81,13 @@ var swiper = new Swiper(".review-slider", {
         	},
         },
       });
+
+function loader(){
+  document.querySelector('.loader-container').classList.add('fade-out');
+}
+
+function fadeOut(){
+  setInterval(loader, 3000);
+}
+
+window.onload = fadeOut;
